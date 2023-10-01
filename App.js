6357e070e07b1b447c3ca80836da28cf5bf14e26
Jonna Huotari <t1hujo04@students.oamk.fi>
2023-10-01 12:02:05 +0300

    valmis

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import DATA from './Data'
+import Row from './components/Row'
+import { FlatList, SafeAreaView, StyleSheet, Text } from "react-native"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return(
+    <SafeAreaView style={styles.container}>
+        <FlatList
+        data={DATA}
+        renderItem={({item}) =>(
+          //<Text>{item.lastname}</Text>
+          <Row person={item}/>
+        )}
+
+       
+
+        ></FlatList>
+    </SafeAreaView>
+)
+
+
+/*function renderItem({item}) {
+    return(<Text>{item.lastname}</Text>)
+}*/
+
+/*const renderItem = ({item}) => (
+  <Text>{item.lastname}</Text>
+)*/
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop:40,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'left',
   },
 });
